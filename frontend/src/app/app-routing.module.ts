2a94c5450ae0478a283d1fc0f17ebb4a350651ec
Ken <kenediidali@gmail.com>
2023-02-15 06:53:30 +0100

    setup frontend

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,24 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+import {LoginComponent} from "./components/login/login.component";
+import {SignupComponent} from "./components/signup/signup.component";
+import {HomeComponent} from "./components/home/home.component";
+import {PostsComponent} from "./components/posts/posts.component";
+
+// setting up navigation routes
+const routes: Routes = [
+  {path: "", component: HomeComponent},
+  {path: "posts", component: PostsComponent},
+  {path: "login", component: LoginComponent},
+  {path: "signup", component: SignupComponent},
+  {path: "**", redirectTo: ""},
+];
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,6 +3,7 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
 
 import {MatButtonModule} from "@angular/material/button";
 import {MatCardModule} from "@angular/material/card";
@@ -11,11 +12,20 @@ import {MatInputModule} from "@angular/material/input";
 import {MatListModule} from "@angular/material/list";
 import {MatToolbarModule} from "@angular/material/toolbar";
 import { NavigationComponent } from './components/navigation/navigation.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { PostsComponent } from './components/posts/posts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    SignupComponent,
+    LoginComponent,
+    HomeComponent,
+    PostsComponent,
+
   ],
   imports: [
     BrowserModule,
@@ -25,7 +35,8 @@ import { NavigationComponent } from './components/navigation/navigation.componen
     MatButtonModule,
     MatCardModule,
     MatIconModule,
-    MatInputModule
+    MatInputModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
